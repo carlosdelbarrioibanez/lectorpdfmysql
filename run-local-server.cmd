1f50ed89bf8331fe8da7451e33f3be0ce68c1144
Carlos del Barrio <cbi0003@alu.medac.es>
2026-03-26 09:16:01 +0100

    Actualizo proyecto

---
+++ run-local-server.cmd
@@ -0,0 +1,3 @@
+@echo off
+cd /d C:\Users\PC02\Desktop\phplectortexto
+"C:\Program Files\php-8.3.4-nts-Win32-vs16-x64\php.exe" -c "C:\Users\PC02\Desktop\phplectortexto\php.local.ini" -S 127.0.0.1:8000 -t "C:\Users\PC02\Desktop\phplectortexto\public"
